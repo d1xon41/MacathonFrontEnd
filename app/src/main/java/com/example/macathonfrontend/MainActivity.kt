4fcbb8c277995cb22be76fbbb5a6c259177d92bc
d1xon41 <imdix0nlim@gmail.com>
2026-04-11 09:52:26 +1000

    First commit

--- app/src/main/java/com/example/macathonfrontend/MainActivity.kt
+++ app/src/main/java/com/example/macathonfrontend/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Task submitted", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
